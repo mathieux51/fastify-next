0f73f165c1f91ada302e356ea5c1d1b684ba7bac
Mathieu <mathieux51@gmail.com>
2019-01-21 09:01:25 +0100

    Update Header

--- components/Header.js
+++ components/Header.js
@@ -2,41 +2,43 @@ import Link from "next/link"
 import styled from "styled-components"
 
 const Nav = styled.nav.attrs({ className: "" })`
-  height: 70px;
   border-bottom: 1px solid ${({ theme }) => theme.g86};
   margin: 0 20px;
   padding: 15px 0;
 `
-const Ul = styled.ul.attrs({ className: "flex" })``
-const Li = styled.ul.attrs({ className: "flex-1" })``
+const Ul = styled.ul.attrs({ className: "" })``
+const Li = styled.li.attrs({ className: "" })``
 const Button = styled.button.attrs({
-  className: "w100 h100 f16 ff-futura-pt ttu"
+  className: "w100 f16 ff-futura-pt ttu"
 })`
   color: ${({ theme }) => theme.g60};
+  height: 70px;
 `
 
-const List = (
+const List = () => (
   <Ul>
     <Li>
       <Link prefetch href="/">
         <a>Home</a>
       </Link>
     </Li>
-    <Li>
-      <Link prefetch href="/about">
-        <a>About</a>
-      </Link>
-    </Li>
-    <Li>
-      <Link prefetch href="/contact">
-        <a>Contact</a>
-      </Link>
-    </Li>
   </Ul>
 )
 
-export default () => (
-  <Nav>
-    <Button>Menu</Button>
-  </Nav>
-)
+export default class extends React.PureComponent {
+  state = {
+    isOpen: false
+  }
+
+  handleOnClick = () => this.setState({ isOpen: !this.state.isOpen })
+
+  render() {
+    const { isOpen } = this.state
+    return (
+      <Nav>
+        {isOpen && <List />}
+        <Button onClick={this.handleOnClick}>Menu</Button>
+      </Nav>
+    )
+  }
+}
