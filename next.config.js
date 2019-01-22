9c109ae674a2e5d455a8832404451fed462b92d3
Mathieu <mathieux51@gmail.com>
2019-01-22 07:38:00 +0100

    Fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const withFonts = require("next-fonts")
 
 module.exports = withFonts({
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? `/{${process.env.npm_package_name}}`
-      : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? `/reponame` : ""
 })
