594d29b0a1614808dfd844bffb4e670cf6e9f9c6
Mathieu <mathieux51@gmail.com>
2019-01-22 20:42:12 +0100

    Test deploy

--- env-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "process.env.BACKEND_URL":
-    process.env.NODE_ENV === "production"
-      ? `/${process.env.npm_package_name}`
-      : ""
-}
--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = withFonts({
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? `/${process.env.npm_package_name}/`
+      : "",
+  "process.env.BACKEND_URL":
+    process.env.NODE_ENV === "production"
+      ? `/${process.env.npm_package_name}`
       : ""
 })
