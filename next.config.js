753be0368be014906244660ccd052d56e27eaa3a
Mathieu <mathieux51@gmail.com>
2019-01-22 21:49:28 +0100

    Test prod

--- next.config.js
+++ next.config.js
@@ -4,9 +4,5 @@ module.exports = withFonts({
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? `/${process.env.npm_package_name}/`
-      : "",
-  "process.env.BACKEND_URL":
-    process.env.NODE_ENV === "production"
-      ? `/${process.env.npm_package_name}`
       : ""
 })
