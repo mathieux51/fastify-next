f817bab46ed5b8f6e48d06ac18685ac05bb87082
Mathieu <mathieux51@gmail.com>
2019-01-24 08:33:30 +0100

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.5fd891c4a63e663bb634.hot-update.js
@@ -0,0 +1,41 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/GlobalStyle.js":
+/*!***********************************!*\
+  !*** ./components/GlobalStyle.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _FontFaces__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./FontFaces */ "./components/FontFaces.js");
+/* harmony import */ var polished__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! polished */ "./node_modules/polished/dist/polished.es.js");
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  ", "\n  ", "\n  body {\n    color: ", ";\n\t\tfont-family: sans-serif;\n    background: ", "\n  }\n\t.fonts-loaded body {\n\t\tfont-family: proxima-nova, sans-serif;\n\t}\n\n  .fonts-loaded .ff-futura-pt {\n    font-family: futura-pt, sans-serif;\n  }\n\n  /* Reset */\n  button {\n    border: 0;\n    padding: 0;\n    background: transparent;\n  }\n\n  ul {\n    list-style: none;\n  }\n\n  .flex {\n    display: flex;\n  }\n  .fxd-c {\n    flex-direction: column;\n  }\n  .flex-1 {\n    flex: 1;\n  }\n  .w100 {\n    width: 100%;\n  }\n  .h100 {\n    height: 100%;\n  }\n  /* font-size */\n  .f12 {\n    font-size: 12px;\n  }\n  .f16 {\n    font-size: 16px;\n  }\n  .f31 {\n    font-size: 31px;\n  }\n\n  .fw300 {\n    font-weight: 300;\n  }\n  .fw400 {\n    font-weight: 400;\n  }\n\n  /* Text */\n  .ttu {\n    text-transform: uppercase;\n  }\n  .tdn {\n    text-decoration: none;\n  }\n  .ta-c {\n    text-align: center;\n  }\n  /* margin/padding */\n  .m0 {\n    margin: 0;\n  }\n  .p0 {\n    padding: 0;\n  }\n  /* display */\n  .d {\n    display: block;\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(styled_components__WEBPACK_IMPORTED_MODULE_0__["createGlobalStyle"])(_templateObject(), _FontFaces__WEBPACK_IMPORTED_MODULE_1__["default"], Object(polished__WEBPACK_IMPORTED_MODULE_2__["normalize"])(), function (_ref) {
+  var theme = _ref.theme;
+  return theme.g38;
+}, function (_ref2) {
+  var theme = _ref2.theme;
+  return theme.bg;
+}));
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.5fd891c4a63e663bb634.hot-update.js.map
