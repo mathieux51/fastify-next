514532aceb3aec9d084e7ade066a04fd200bbac3
Mathieu <mathieux51@gmail.com>
2019-01-24 23:04:41 +0100

    Upate font load script and add bundle analyser

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 const withFonts = require("next-fonts")
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
-module.exports = withFonts({
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? `/${process.env.npm_package_name}/`
-      : ""
-})
+module.exports = withBundleAnalyzer(
+  withFonts({
+    // withFonts
+    assetPrefix:
+      process.env.NODE_ENV === "production"
+        ? `/${process.env.npm_package_name}/`
+        : "",
+    // withBundleAnalyzer
+    analyzeServer: ["server", "all"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["web", "all"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/server.html"
+      },
+      browser: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/client.html"
+      }
+    }
+  })
+)
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,12 @@
-import React, { Fragment } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import Header from '../components/Header'
-import Head from '../components/Head'
-import Body from '../components/Body'
-import GlobalStyle from '../components/GlobalStyle'
-import theme from '../theme'
+import React, { Fragment } from "react"
+import styled, { ThemeProvider } from "styled-components"
+import Header from "../components/Header"
+import Head from "../components/Head"
+import Body from "../components/Body"
+import GlobalStyle from "../components/GlobalStyle"
+import theme from "../theme"
 
 export default class extends React.PureComponent {
-  async componentDidMount() {
-    if (sessionStorage.fontsLoadedFoutWithClass) {
-      document.documentElement.className += ' fonts-loaded'
-      return
-    }
-    if ('fonts' in document) {
-      await Promise.all([
-        document.fonts.load('italic 100 1em proxima-nova'),
-        document.fonts.load('italic 300 1em futura-pt'),
-        document.fonts.load('italic 400 1em futura-pt'),
-        document.fonts.load('italic 700 1em futura-pt'),
-        document.fonts.load('italic 700 1em proxima-nova'),
-        document.fonts.load('normal 100 1em proxima-nova'),
-        document.fonts.load('normal 300 1em futura-pt'),
-        document.fonts.load('normal 300 1em proxima-nova'),
-        document.fonts.load('normal 400 1em futura-pt'),
-        document.fonts.load('normal 400 1em proxima-nova'),
-        document.fonts.load('normal 700 1em futura-pt'),
-        document.fonts.load('normal 700 1em proxima-nova')
-      ])
-      document.documentElement.className += ' fonts-loaded'
-      sessionStorage.fontsLoadedFoutWithClass = true
-    }
-  }
   render() {
     return (
       <ThemeProvider theme={theme}>
