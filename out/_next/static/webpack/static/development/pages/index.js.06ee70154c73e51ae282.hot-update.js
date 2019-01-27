305b97ddf4e5e3d4a3f25e51763069571128a7e9
Mathieu <mathieux51@gmail.com>
2019-01-27 17:46:03 +0100

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.06ee70154c73e51ae282.hot-update.js
@@ -0,0 +1,109 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./assets/images/manu/1.jpg":
+/*!**********************************!*\
+  !*** ./assets/images/manu/1.jpg ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/1-e210c7b54a7f980bf87e4b41ba2a395c.jpg";
+
+/***/ }),
+
+/***/ "./assets/images/manu/2.jpg":
+/*!**********************************!*\
+  !*** ./assets/images/manu/2.jpg ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/2-4b4688de4a03d1935c2aa38f4c596140.jpg";
+
+/***/ }),
+
+/***/ "./assets/images/manu/3.png":
+/*!**********************************!*\
+  !*** ./assets/images/manu/3.png ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/3-b8b22158394fd2319d369568fd9d306c.png";
+
+/***/ }),
+
+/***/ "./assets/images/manu/thumbnail.jpg":
+/*!******************************************!*\
+  !*** ./assets/images/manu/thumbnail.jpg ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/thumbnail-4b03125ca16038738ea8e1377e0fb83f.jpg";
+
+/***/ }),
+
+/***/ "./db/index.js":
+/*!*********************!*\
+  !*** ./db/index.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _projects_helvetica__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./projects/helvetica */ "./db/projects/helvetica.js");
+/* harmony import */ var _projects_papicha__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects/papicha */ "./db/projects/papicha.js");
+/* harmony import */ var _projects_manu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./projects/manu */ "./db/projects/manu.js");
+
+
+ // import garcon from "./projects/garcon"
+// import melanie from "./projects/melanie"
+// // import amoureuses from "./projects/amoureuses"
+
+var projects = [_projects_helvetica__WEBPACK_IMPORTED_MODULE_0__["default"], _projects_papicha__WEBPACK_IMPORTED_MODULE_1__["default"], _projects_manu__WEBPACK_IMPORTED_MODULE_2__["default"]];
+/* harmony default export */ __webpack_exports__["default"] = ({
+  projects: projects
+});
+
+/***/ }),
+
+/***/ "./db/projects/manu.js":
+/*!*****************************!*\
+  !*** ./db/projects/manu.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _assets_images_manu_thumbnail_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../assets/images/manu/thumbnail.jpg */ "./assets/images/manu/thumbnail.jpg");
+/* harmony import */ var _assets_images_manu_thumbnail_jpg__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_assets_images_manu_thumbnail_jpg__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _assets_images_manu_1_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../assets/images/manu/1.jpg */ "./assets/images/manu/1.jpg");
+/* harmony import */ var _assets_images_manu_1_jpg__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_assets_images_manu_1_jpg__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _assets_images_manu_2_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../assets/images/manu/2.jpg */ "./assets/images/manu/2.jpg");
+/* harmony import */ var _assets_images_manu_2_jpg__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_assets_images_manu_2_jpg__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _assets_images_manu_3_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../assets/images/manu/3.png */ "./assets/images/manu/3.png");
+/* harmony import */ var _assets_images_manu_3_png__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_assets_images_manu_3_png__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  title: "manu",
+  href: "/manu",
+  category: [],
+  photos: [_assets_images_manu_1_jpg__WEBPACK_IMPORTED_MODULE_1___default.a, _assets_images_manu_2_jpg__WEBPACK_IMPORTED_MODULE_2___default.a, _assets_images_manu_3_png__WEBPACK_IMPORTED_MODULE_3___default.a],
+  video: {
+    type: "vimeo",
+    videoId: "264683681"
+  },
+  thumbnail: _assets_images_manu_thumbnail_jpg__WEBPACK_IMPORTED_MODULE_0___default.a,
+  description: "A documentary by Emmanuelle Bonmariage\n\nCinematography shared with Joachim Philippe & Chris Renson\n\nProd: Clin d'oeil Films\n\nShot in Belgium / France \n\nSony FS7\n\n90'\n\n2017 \n\n\u2014\u2014\n\n* Shortlist Magritte du Cin\xE9ma 2019 * "
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.06ee70154c73e51ae282.hot-update.js.map
