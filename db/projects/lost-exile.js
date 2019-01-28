905a4b7628385c2f77059f9a2d52fd111140a4c5
Mathieu <mathieux51@gmail.com>
2019-01-28 23:10:14 +0100

    Update db

---
+++ db/projects/lost-exile.js
@@ -0,0 +1,59 @@
+import thumbnail from "../../assets/images/lost-exile/thumbnail.jpg"
+import p1 from "../../assets/images/lost-exile/1.jpg"
+import p2 from "../../assets/images/lost-exile/2.jpg"
+import p3 from "../../assets/images/lost-exile/3.jpg"
+import p4 from "../../assets/images/lost-exile/4.jpg"
+import p5 from "../../assets/images/lost-exile/5.jpg"
+import p6 from "../../assets/images/lost-exile/6.jpg"
+import p7 from "../../assets/images/lost-exile/7.jpg"
+import p8 from "../../assets/images/lost-exile/8.jpg"
+import p9 from "../../assets/images/lost-exile/9.jpg"
+
+export default {
+  title: "Lost Exile",
+  href: "/lost-exile",
+  category: [],
+  thumbnail,
+  photos: [p1, p2, p3, p4, p5, p6, p7, p8, p9],
+  videos: [
+    {
+      type: "vimeo",
+      videoId: "198256811"
+    }
+  ],
+  description: ` A short film by Fisnik Maxhuni
+
+  Prod: Alva Films / Ikone Studio
+  
+  Shot in Kosovo
+  
+  Arri Alexa Plus + Zeiss T2.1
+  
+  29'
+  
+  2016
+  
+  ----
+  
+  Camera assistant: Alexandre Cabanne
+  
+  Gaffer: Astrit Bica
+  
+  Colorist: Loup Brenta
+  
+  ----
+  
+  Best Film - Ouchy Film AWARDS, Lausanne, 2016
+  
+  
+  Prix de la région Auvergne Rhône-Alpes - Villeurbanne 2016
+  
+  
+  Best Albanian short film - Tirana International Film Festival, Albania 2016
+  
+  
+  Best Short Film - New Delhi Shorts International Film Festival 2016
+  
+  
+  Mention spéciale Concorso nazionale Locarno film festival 2016`
+}
---
+++ db/projects/valet-noir.js
@@ -0,0 +1,42 @@
+import thumbnail from "../../assets/images/valet-noir/thumbnail.jpg"
+import p1 from "../../assets/images/valet-noir/1.jpg"
+import p2 from "../../assets/images/valet-noir/2.jpg"
+import p3 from "../../assets/images/valet-noir/3.jpg"
+import p4 from "../../assets/images/valet-noir/4.jpg"
+import p5 from "../../assets/images/valet-noir/5.jpg"
+
+export default {
+  title: "Valet Noir",
+  href: "/valet-noir",
+  category: [],
+  thumbnail,
+  photos: [p1, p2, p3, p4, p5],
+  videos: null,
+  description: `A short film by Lora Mure-Ravaud
+
+  Prod: ALVA FILMS
+  
+  Shot in Switzerland / France
+  
+  Arri Alexa Mini + Ultraprimes
+  
+  22'
+  
+  2017
+  
+  ----
+  
+  Camera assistant: Guillaume Brandt
+  
+  Gaffer: Antonin Suppes
+  
+  Colorist: Loup Brenta 
+  
+  —
+  
+  Awards:
+  
+  1st Prize Upcoming talent at Soleure festival 2018
+  
+  Best Swiss Short award at the Kurzfilmtage Winterthur in 2017`
+}
