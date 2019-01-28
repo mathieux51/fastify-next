3a9d5b835063c60af13d84b8b94b6230ea6c6b5a
Mathieu <mathieux51@gmail.com>
2019-01-28 23:50:25 +0100

    Update db

--- db/projects/scarlett-o-hanna.js
+++ db/projects/scarlett-o-hanna.js
@@ -1,16 +1,39 @@
-import thumbnail from "../../assets/images/les-amoureuses/thumbnail.jpg"
+import thumbnail from "../../assets/images/scarlett-o-hanna/thumbnail.jpg"
 
 export default {
-  title: "Les amoureuses",
-  href: "/amoureuses",
+  title: "Scarlett O'Hanna",
+  href: "/scarlett-o-hanna",
   category: [],
   thumbnail,
   photos: null,
   videos: [
     {
       type: "vimeo",
-      videoId: "198032798"
+      videoId: "78256558"
+    },
+    {
+      type: "vimeo",
+      videoId: "51248767"
     }
   ],
-  description: ``
+  description: `- THIEVES -
+
+  A music video by Scarlett O'Hanna & Léo Lefèvre
+  
+  Arri Alexa XT 4:3 / Phantom Miro M320s + Kowa anamorphic
+  
+  2013
+  
+  _______________________________________________
+  
+  
+  - ONE SUNDAY MORNING -
+  
+  a music video by Léo Lefèvre
+  
+  16mm
+  
+  Arri SR3 + Canon zoom lens
+  
+  2012`
 }
---
+++ db/projects/woodie-smalls.js
@@ -0,0 +1,35 @@
+import thumbnail from "../../assets/images/woodie-smalls/thumbnail.jpg"
+import p1 from "../../assets/images/woodie-smalls/1.jpg"
+
+export default {
+  title: "Woodie Smalls",
+  href: "/woodie-smalls",
+  category: [],
+  thumbnail,
+  photos: [p1],
+  videos: [
+    {
+      type: "vimeo",
+      videoId: "173619922"
+    }
+  ],
+  description: `- Planet Shrooms -
+
+  Directed by Alice Khol
+  
+  Cinematography: Léo Lefèvre & Son Doan
+  
+  Shot on Arri Alexa Mini / Sony A7sII + Leica Van Diemen & Cooke Zoom
+  
+  2016
+  
+  ----
+  
+  Camera assistant: Alexandre Cabanne
+  
+  Ronin operator: Geoffroy Minne
+  
+  Grip & electric: Jérémy Bourgeois et Julien Ramirez
+  
+  Colorist: Paul Millot`
+}
