f66a18cc7e1bfae77370edabbdd732a9f21efba5
Mathieu <mathieux51@gmail.com>
2019-01-28 08:00:43 +0100

    Update db

---
+++ db/projects/scarlett-o-hanna.js
@@ -0,0 +1,16 @@
+import thumbnail from "../../assets/images/les-amoureuses/thumbnail.jpg"
+
+export default {
+  title: "Les amoureuses",
+  href: "/amoureuses",
+  category: [],
+  thumbnail,
+  photos: null,
+  videos: [
+    {
+      type: "vimeo",
+      videoId: "198032798"
+    }
+  ],
+  description: ``
+}
