4d66ce0133c9234567daaf9a19b8231131819746
Mathieu <mathieux51@gmail.com>
2019-01-29 07:56:44 +0100

    Plug db

--- db/index.js
+++ db/index.js
@@ -1,21 +1,45 @@
-import helvetica from "./projects/helvetica"
-import papicha from "./projects/papicha"
-import manu from "./projects/manu"
-import garcon from "./projects/garcon"
-import melanieDeBiasio from "./projects/melanie-de-biasio"
-import lesAmoureuses from "./projects/les-amoureuses"
 import christinaVantzou from "./projects/christina-vantzou"
+import deMonCoeur from "./projects/de-mon-coeur-froid"
+import foulek from "./projects/foulek"
+import garcon from "./projects/garcon"
+import globetrotteur from "./projects/globetrotteur"
+import helvetica from "./projects/helvetica"
+import jeLesAimeTous from "./projects/je-les-aime-tous"
+import leMalDuCitron from "./projects/le-mal-du-citron"
+import leSkeletonBand from "./projects/le-skeleton-band"
+import lesAmoureuses from "./projects/les-amoureuses"
+import lightMyFire from "./projects/light-my-fire"
+import lostExile from "./projects/lost-exile"
+import manu from "./projects/manu"
+import melanieDeBiasio from "./projects/melanie-de-biasio"
+import morella from "./projects/morella"
+import papicha from "./projects/papicha"
+import scarlettOHanna from "./projects/scarlett-o-hanna"
 import theHiddenPart from "./projects/the-hidden-part"
+import valetNoir from "./projects/valet-noir"
+import woodieSmalls from "./projects/woodie-smalls"
 
 const projects = [
-  helvetica,
-  papicha,
-  manu,
-  garcon,
-  melanieDeBiasio,
-  lesAmoureuses,
   christinaVantzou,
-  theHiddenPart
+  deMonCoeur,
+  foulek,
+  garcon,
+  globetrotteur,
+  helvetica,
+  jeLesAimeTous,
+  leMalDuCitron,
+  leSkeletonBand,
+  lesAmoureuses,
+  lightMyFire,
+  lostExile,
+  manu,
+  melanieDeBiasio,
+  morella,
+  papicha,
+  scarlettOHanna,
+  theHiddenPart,
+  valetNoir,
+  woodieSmalls
 ]
 
 export default {
--- db/projects/je-les-aime-tous.js
+++ db/projects/je-les-aime-tous.js
@@ -5,9 +5,10 @@ import p3 from "../../assets/images/je-les-aime-tous/3.jpg"
 import p4 from "../../assets/images/je-les-aime-tous/4.jpg"
 import p5 from "../../assets/images/je-les-aime-tous/5.jpg"
 import p6 from "../../assets/images/je-les-aime-tous/6.jpg"
-import p7 from "../../assets/images/je-les-aime-tous/8.jpg"
-import p7 from "../../assets/images/je-les-aime-tous/9.jpg"
-import p7 from "../../assets/images/je-les-aime-tous/10.jpg"
+import p7 from "../../assets/images/je-les-aime-tous/7.jpg"
+import p8 from "../../assets/images/je-les-aime-tous/8.jpg"
+import p9 from "../../assets/images/je-les-aime-tous/9.jpg"
+import p10 from "../../assets/images/je-les-aime-tous/10.jpg"
 
 export default {
   title: "Je les aime tous",
