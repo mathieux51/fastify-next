aa67c79532100958929f24d77cbdd391ab7005bf
Mathieu <mathieux51@gmail.com>
2019-01-31 07:50:20 +0100

    Remove prefetch on Link

--- components/Link.js
+++ components/Link.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import styled from "styled-components"
 
 export default ({ href, className, children }) => (
-  <Link prefetch href={href}>
+  <Link href={href}>
     <a className={className}>{children}</a>
   </Link>
 )
