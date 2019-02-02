48cb8e0a14452254efb7af82f1587f75b74b992f
Mathieu <mathieux51@gmail.com>
2019-02-02 13:39:36 +0100

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.2bba7f90034d8eb6af1d.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/VimeoIcon.js":
+/*!*********************************!*\
+  !*** ./components/VimeoIcon.js ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/mathieu/Projects/learning/react/fastify-next/components/VimeoIcon.js";
+
+/* harmony default export */ __webpack_exports__["default"] = (function (_ref) {
+  var className = _ref.className;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    viewBox: "0 0 64 64",
+    width: "100%",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+    d: "M47,25c-0.1,2.9-2.2,6.9-6.1,12c-4.1,5.3-7.5,8-10.4,8c-1.7,0-3.2-1.6-4.4-4.8 c-0.8-3-1.6-5.9-2.4-8.9c-0.9-3.2-1.9-4.8-2.9-4.8c-0.2,0-1,0.5-2.4,1.4L17,26c1.5-1.3,2.9-2.6,4.4-3.9c2-1.7,3.5-2.6,4.4-2.7 c2.3-0.2,3.8,1.4,4.3,4.8c0.6,3.7,1,6,1.2,6.9c0.7,3.1,1.4,4.6,2.2,4.6c0.6,0,1.6-1,2.8-3c1.3-2,1.9-3.5,2-4.5 c0.2-1.7-0.5-2.6-2-2.6c-0.7,0-1.5,0.2-2.2,0.5c1.5-4.8,4.3-7.2,8.4-7C45.7,19.1,47.2,21.1,47,25z",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.2bba7f90034d8eb6af1d.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.3c2d6f5f3800bb844446.hot-update.js
@@ -0,0 +1,40 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/ImdbIcon.js":
+/*!********************************!*\
+  !*** ./components/ImdbIcon.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/mathieu/Projects/learning/react/fastify-next/components/ImdbIcon.js";
+
+/* harmony default export */ __webpack_exports__["default"] = (function (_ref) {
+  var className = _ref.className;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    viewBox: "16 16 30 30",
+    width: "100%",
+    className: className,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+    d: "M43.91,20H20.09C17.831,20,16,21.831,16,24.09v14.82c0,2.259,1.831,4.09,4.09,4.09h23.82c2.259,0,4.09-1.831,4.09-4.09V24.09C48,21.831,46.169,20,43.91,20z M46,38.91c0,1.152-0.938,2.09-2.09,2.09H20.09C18.938,41,18,40.062,18,38.91V24.09c0-1.152,0.938-2.09,2.09-2.09h23.82c1.152,0,2.09,0.938,2.09,2.09V38.91z M20,35h1.337v-7.999H20V35z M26.321,30.669l-2.057-3.668h-1.382V35h1.291v-5.645l2.114,3.691h0.035l2.126-3.702V35h1.313v-7.999h-1.406L26.321,30.669zM33.751,27.001h-2.446V35h2.446c2.285,0,3.794-1.611,3.794-3.954v-0.103C37.545,28.6,36.037,27.001,33.751,27.001z M36.174,31.035c0,1.725-0.982,2.754-2.433,2.754h-1.109v-5.588h1.109c1.452,0,2.433,1.062,2.433,2.777V31.035z M41.646,28.829c-0.857,0-1.394,0.503-1.771,1.086v-3.166h-1.291V35h1.291v-0.914c0.366,0.525,0.903,1.028,1.771,1.028c1.223,0,2.354-1.017,2.354-3.074v-0.148C44,29.846,42.857,28.829,41.646,28.829z M42.698,32.029c0,1.211-0.618,1.942-1.406,1.942c-0.777,0-1.44-0.754-1.44-1.942v-0.114c0-1.189,0.663-1.943,1.44-1.943c0.777,0,1.406,0.743,1.406,1.954V32.029z",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.3c2d6f5f3800bb844446.hot-update.js.map
