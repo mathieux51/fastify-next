e87901f3f2d1ee46e029abb26606b1b31642fc44
Mathieu <mathieux51@gmail.com>
2019-02-02 18:16:25 +0100

    Fix Header on desktop

--- .babelrc.js
+++ .babelrc.js
@@ -7,6 +7,7 @@ module.exports = {
         root: ["./"],
         alias: {
           style: "./style",
+          helpers: "./helpers",
           components: "./components"
         }
       }
--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,77 @@
 import { Fragment } from "react"
 import styled, { css } from "styled-components"
 
+import { media } from "helpers"
 import Link from "./Link"
+
+const Container = styled.div.attrs({ className: "flex" })`
+  flex-direction: column;
+  margin: 20px 20px 0;
+  ${media.desktop`
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom: 1px solid ${({ theme }) => theme.g86};  
+  `}
+`
 
 const Nav = styled.nav.attrs({ className: "" })`
   border-bottom: 1px solid ${({ theme }) => theme.g86};
   margin: 15px 20px;
+  ${media.desktop`
+    border: 0;
+    overflow-x: scroll;
+  `}
 `
 const StyledLink = styled(Link).attrs({
-  className: "ff-futura-pt f16 ttu tdn ta-c w100 d"
+  className: "ff-futura-pt ttu tdn ta-c w100"
 })`
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+
   letter-spacing: 1px;
   line-height: 2.5em;
   color: ${({ theme }) => theme.g60};
+  font-size: 16px;
+  ${media.desktop`
+    font-size: 12px;
+  `}
 `
 
-const Ul = styled.ul.attrs({ className: "fxd-c m0 p0" })`
+const Title = styled.div.attrs({
+  className: ""
+})`
   border-bottom: 1px solid ${({ theme }) => theme.g86};
+  margin: 0 20px;
+  ${media.desktop`
+    order: -1;
+    border: 0;
+        
+  `}
+`
+
+const Ul = styled.ul.attrs({ className: "m0 p0" })`
   padding-bottom: 15px;
+  display: ${({ isOpen }) => (!isOpen ? "none" : "inherit")};
+  ${media.desktop`
+    display: flex;
+    padding: 0;
+    border: 0;
+    & > li {
+      margin-right: 28px;
+    }
+    & > li:last-child {
+      margin-right: 0;
+    }
+  `}
 `
 const Button = styled(StyledLink).attrs({
   as: "button"
 })`
   height: 70px;
+  ${media.desktop`
+    display: none;
+  `}
 `
 
 const ListItem = ({ href, txt }) => (
@@ -31,8 +80,8 @@ const ListItem = ({ href, txt }) => (
   </li>
 )
 
-const List = () => (
-  <Ul>
+const List = ({ isOpen }) => (
+  <Ul isOpen={isOpen}>
     <ListItem href="/" txt="home" />
     <ListItem href="/feature-tv" txt="features / tv" />
     <ListItem href="/documentaries" txt="documentary" />
@@ -44,17 +93,14 @@ const List = () => (
   </Ul>
 )
 
-const Container = styled.div.attrs({
-  className: ""
-})`
-  border-bottom: 1px solid ${({ theme }) => theme.g86};
-  margin: 0 20px;
-`
-
 const H1 = styled.h1.attrs({
-  className: "ff-futura-pt m0 f31 fw300 ttu ta-c"
+  className: "ff-futura-pt m0 f31 fw300 ttu"
 })`
   color: ${({ theme }) => theme.g13};
+  text-align: center;
+  ${media.desktop`
+    text-align: inherit;
+  `}
 `
 
 const H2 = styled.h2.attrs({
@@ -74,16 +120,16 @@ export default class extends React.PureComponent {
   render() {
     const { isOpen } = this.state
     return (
-      <Fragment>
+      <Container>
         <Nav id="nav">
-          {isOpen && <List />}
+          {<List isOpen={isOpen} />}
           <Button onClick={this.handleOnClick}>Menu</Button>
         </Nav>
-        <Container>
+        <Title>
           <H1>Léo Lefèvre</H1>
           <H2>Cinematographer</H2>
-        </Container>
-      </Fragment>
+        </Title>
+      </Container>
     )
   }
 }
