295d8f158b6e03a0af84a4ffaa04f2674dd5ca1a
Mathieu <mathieux51@gmail.com>
2019-02-04 08:04:13 +0100

    WIP react-plyr

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,23 @@
 import React from "react"
 import { withRouter } from "next/router"
+import styled from "styled-components"
+import Plyr from "react-plyr"
+// 🚧 Create a global style for this 🚧
+// import "plyr/dist/plyr.css"
 
 import { ProjectConsumer } from "components/ProjectContext"
 
-const Projects = props =>
-  console.log(props) || <h1>{props.router.query.name}</h1>
+const Container = styled.div``
+
+const Projects = props => {
+  const [cur] = props.projects.filter(p => p.href === props.router.query.name)
+  return (
+    <Container>
+      {cur.videos &&
+        cur.videos.map(v => <Plyr type={v.type} videoId={v.videoId} />)}
+    </Container>
+  )
+}
 
 export default withRouter(props => (
   <ProjectConsumer>
